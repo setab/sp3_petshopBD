b1941664ff561a43420957576fe8d292f2bdad27
setab <saidulislamsetab@gmail.com>
2024-05-21 17:19:02 +0600

    added new html and css file for contact

--- main.js
+++ main.js
@@ -1,29 +1,12 @@
-// const url = "https://api.thecatapi.com/v1/images/search";
-// const section = document.querySelector(".cat_pics");
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-// async function getCatPic() {
-//   section.innerHTML = "";
-//   try {
-//     const response = await fetch(url);
-//     const json = await response.json();
-//     console.log("Json:", json);
-//     if (json && json.length > 0) {
-//       let photo = json[0].url;
-//       section.classList.add("cats");
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
 
-//       let img = document.createElement("img");
-//       img.src = photo;
-//       img.classList.add("random_cats");
-//       img.alt = photo;
-//       section.appendChild(img);
-//     } else {
-//       console.log("No cat pictures found");
-//     }
-//   } catch (error) {
-//     console.error("Error fetching cat picture:", error);
-//   }
-// }
-
-// for (let i = 0; i < 4; i++) {
-//   getCatPic();
-// }
+    targetElement.scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
