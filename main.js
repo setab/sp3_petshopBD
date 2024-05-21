109713d5d93e8633ad4eea9e146332b9ad86b150
setab <saidulislamsetab@gmail.com>
2024-05-21 18:11:28 +0600

    added progress bar animation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// smooth scrool down animation
+
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -10,3 +12,40 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+// progress bar animation
+
+function animateProgressBar(
+  barSelector,
+  percentSelector,
+  targetValue,
+  duration
+) {
+  const progressBar = document.querySelector(barSelector);
+  const percentNum = document.querySelector(percentSelector);
+
+  let start = null;
+  const initialValue = 0;
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    const progressPercentage = Math.min(
+      initialValue + (progress / duration) * targetValue,
+      targetValue
+    );
+
+    progressBar.style.width = progressPercentage + "%";
+    percentNum.textContent = Math.round(progressPercentage) + "%";
+
+    if (progressPercentage < targetValue) {
+      window.requestAnimationFrame(step);
+    }
+  }
+
+  window.requestAnimationFrame(step);
+}
+
+// Start
+animateProgressBar(".progress-bar-1", "#percent-num-1", 95, 2000); // 2 seconds duration
+animateProgressBar(".progress-bar-2", "#percent-num-2", 75, 2000); // 2 seconds duration
